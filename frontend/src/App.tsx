c214104fd23d9c415fc97268bf4683e35ae28872
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2024-04-02 00:38:56 -0500

    MVP ready

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import { ChangeEvent, MouseEvent, useState } from "react";
-import "./App.css";
+
+type APIResponse = {
+  mosaicImg: string;
+  duration: number;
+};
 
 function App() {
   const [file, setFile] = useState<File>();
+  const [tileSize, setTileSize] = useState<string>("10");
+
+  const [mosaicImg, setMosaicImg] = useState<string>();
+  const [duration, setDuration] = useState<number>();
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const target = event.target;
@@ -11,7 +19,15 @@ function App() {
     }
   }
 
-  const handleResponse = () => {};
+  const handleResponse = async (response: Response) => {
+    try {
+      const data = (await response.json()) as APIResponse;
+      setMosaicImg(data.mosaicImg);
+      setDuration(data.duration);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const handleError = () => {};
 
   const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
@@ -19,28 +35,79 @@ function App() {
     if (file) {
       const url = "/api/file/upload";
       const formData = new FormData();
-      formData.append("file", file);
+      formData.append("imgUpload", file);
       formData.append("fileName", file.name);
-      const headers = {
-        "content-type": "multipart/form-data",
-      };
-      fetch(url, { headers, method: "POST", body: formData })
+      formData.append("tileSize", tileSize!);
+
+      fetch(url, { method: "POST", body: formData })
         .then(handleResponse)
         .catch(handleError);
     }
   };
 
   return (
-    <div className="App">
-      <form>
-        <input
-          type="file"
-          name="img_upload"
-          id="img_upload"
-          onChange={handleChange}
-        />
-        <button onClick={handleSubmit}>Upload</button>
-      </form>
+    <div className="d-flex justify-content-center mt-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <form>
+              <input
+                type="file"
+                name="imgUpload"
+                id="imgUpload"
+                onChange={handleChange}
+                className="form-control-file"
+              />
+              <div className="form-group">
+                <label htmlFor="tileSize">Select tile size</label>
+                <select
+                  name="tileSize"
+                  id="tileSize"
+                  onChange={(e) => setTileSize(e.target.value)}
+                  value={tileSize}
+                  className="form-control"
+                >
+                  <option value="10" selected>
+                    10
+                  </option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+                  <option value="25">25</option>
+                  <option value="50">50</option>
+                  <option value="100">100</option>
+                </select>
+              </div>
+              <button
+                onClick={handleSubmit}
+                type="submit"
+                className="btn btn-primary"
+              >
+                Upload
+              </button>
+            </form>
+          </div>
+          <div className="col-sm">
+            {mosaicImg && (
+              <div className="d-flex flex-column">
+                <img
+                  src={`data:image/jpeg;base64,${mosaicImg}`}
+                  alt="mosaic"
+                  width="25%"
+                />
+                <span>{duration} seconds</span>
+                <a
+                  href={`data:image/jpeg;base64,${mosaicImg}`}
+                  download={`mosaic-version-${file?.name}`}
+                  className="btn btn-success"
+                  style={{ width: "fit-content" }}
+                >
+                  Download
+                </a>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"image"
 	"image/color"
+	"image/draw"
+	"image/jpeg"
 	"log"
 	"math"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -34,12 +40,70 @@ func getEnvVar(envVar string) string {
 }
 
 func mosaic(w http.ResponseWriter, r *http.Request) {
+	t0 := time.Now()
+	r.ParseMultipartForm(10485760)
+
+	file, _, err := r.FormFile("imgUpload")
+	if err != nil {
+		fmt.Println("error with uploaded file: ", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+
+		data := struct {
+			Error string `json:"error"`
+		}{Error: err.Error()}
+
+		json.NewEncoder(w).Encode(data)
+	}
+
+	defer file.Close()
+	tileSize, _ := strconv.Atoi(r.FormValue("tileSize"))
+	original, _, _ := image.Decode(file)
+	bounds := original.Bounds()
+
+	newImage := image.NewNRGBA(image.Rect(bounds.Min.X, bounds.Min.X, bounds.Max.X, bounds.Max.Y))
+	db := cloneTilesDB()
+
+	sourcePoint := image.Point{0, 0}
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y = y + tileSize {
+		for x := bounds.Min.X; x < bounds.Max.X; x = x + tileSize {
+			r, g, b, _ := original.At(x, y).RGBA()
+			color := [3]float64{float64(r), float64(g), float64(b)}
+			nearestFileByColor := nearest(color, &db)
+			file, err := os.Open(nearestFileByColor)
+			if err == nil {
+				img, _, err := image.Decode(file)
+				if err == nil {
+					t := resize(img, tileSize)
+					tile := t.SubImage(t.Bounds())
+					tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
+					draw.Draw(newImage, tileBounds, tile, sourcePoint, draw.Src)
+				} else {
+					fmt.Println("error: ", err, nearestFileByColor)
+				}
+			} else {
+				fmt.Println("error: ", nearestFileByColor)
+			}
+			file.Close()
+		}
+	}
+
+	buf2 := new(bytes.Buffer)
+	jpeg.Encode(buf2, newImage, nil)
+	mosaicImg := base64.StdEncoding.EncodeToString(buf2.Bytes())
+	t1 := time.Now()
+	secondsDiff := t1.Sub(t0).Seconds()
+	duration := math.Round(secondsDiff*100) / 100
+
 	data := struct {
-		Img      string  `json:"image"`
-		Duration float64 `json:"duration"`
-	}{Img: "fdghhdfd", Duration: 2.36}
+		MosaicImg string  `json:"mosaicImg"`
+		Duration  float64 `json:"duration"`
+	}{MosaicImg: mosaicImg, Duration: duration}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+
 	json.NewEncoder(w).Encode(data)
 }
 
@@ -59,7 +123,7 @@ func averageColor(img image.Image) [3]float64 {
 func resize(in image.Image, newWidth int) image.NRGBA {
 	bounds := in.Bounds()
 	ratio := bounds.Dx() / newWidth
-	out := image.NewNRGBA(image.Rect(bounds.Min.X/ratio, bounds.Min.Y/ratio, bounds.Max.X/ratio, bounds.Max.Y/ratio))
+	out := image.NewNRGBA(image.Rect(bounds.Min.X/ratio, bounds.Min.X/ratio, bounds.Max.X/ratio, bounds.Max.Y/ratio))
 
 	for y, j := bounds.Min.Y, bounds.Min.Y; y < bounds.Max.Y; y, j = y+ratio, j+1 {
 		for x, i := bounds.Min.X, bounds.Min.X; i < bounds.Max.X; x, i = x+ratio, i+1 {
